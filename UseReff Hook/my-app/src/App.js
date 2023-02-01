4b730c97a50c943cbadae02a29e09f710d83f351
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2023-02-01 11:48:38 +0530

    UseReff Completed

--- UseReff Hook/my-app/src/App.js
+++ UseReff Hook/my-app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import {useState,useEffect,useRef} from 'react';
 
 function App() {
+  // const [input,setInput] = useState("");
+  // const counter = useRef(0);
+  const inputValid = useRef()
+
+
+  // useEffect(() => {
+  //  counter.current = counter.current + 1
+  // })
+ 
+  // const formHandler = (e) => {
+  //    setInput(e.target.value)
+  // }
+
+  const getData = () => {
+   console.log(inputValid.current.value) 
+   inputValid.current.style.border = "4px solid blue"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header />
+    <input ref={inputValid} className='inp' value={input} onChange={formHandler} />
+    <button onClick={getData}>Submit</button>
+    <h1>How Many Time It's Render {counter.current}</h1>
+  </div>
   );
 }
 
---
+++ UseReff Hook/my-app/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='ref'>
+      <h1>UseReff Hook</h1>
+    </div>
+  )
+}
+
+export default Header
