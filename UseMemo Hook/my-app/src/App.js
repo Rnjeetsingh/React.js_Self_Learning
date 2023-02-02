854bccb138275254e4287791d1ad7a3205fdacda
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2023-02-02 12:05:28 +0530

    useMemo Hook Completed

--- UseMemo Hook/my-app/src/App.js
+++ UseMemo Hook/my-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useMemo} from "react";
+import Header from './Components/Header';
 
 function App() {
+  
+const [count,setCount] = useState(0);
+const [name,setName] = useState('');
+
+const expansivCalculating = (num) => {
+  for(let i = 0; i < 1000000000000000; i++){
+    return num
+  }
+};
+
+const caluclating = useMemo(() => {
+ expansivCalculating(count)
+},[count]) ;
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+
+    <button onClick={() => setCount(count + 1)}>Click Me</button>
+    <h1>Count : {count}</h1>
+    <input onChange={(e) => setName(e.target.value)} />
+    <h1>Name : {name}</h1>
     </div>
   );
 }
---
+++ UseMemo Hook/my-app/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='memo'>
+      <h1>UseMemo Hook</h1>
+    </div>
+  )
+}
+
+export default Header
