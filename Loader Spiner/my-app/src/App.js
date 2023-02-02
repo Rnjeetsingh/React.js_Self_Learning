3d8ce0c4e22bd216ade4c65de55793d630f8cd98
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2023-02-02 21:01:54 +0530

    Loading Tostify completed

--- Loader Spiner/my-app/src/App.js
+++ Loader Spiner/my-app/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import {TailSpin,ThreeCircles,Bars} from 'react-loader-spinner'
+import { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+const [loader,setLoader] = useState(false);
+const [data,setData] = useState([]);
+
+useEffect(() =>{
+  setLoader(true);
+  async function getData (){
+    const res = await fetch("https://hub.dummyapis.com/employee?noofRecords=400&idStarts=1001");
+    const result = await res.json();
+    console.log(result)
+    setData(result);
+    setLoader(false);
+    toast.success("Succesfully Featch Data")
+  }
+  getData();
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <ToastContainer />
+     <div className='loader'>
+    {
+      loader ? 
+      <ThreeCircles color='red' height={400}/> :
+      data.map((el,i) => {
+        return(
+          <div key={i} >
+          <h1>{el.firstName}</h1>
+          <h1>{el.email}</h1>
+          </div>
+        )
+      })
+    }
+     </div>
     </div>
   );
 }
---
+++ Loader Spiner/my-app/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Loader Spiner</h1>
+    </div>
+  )
+}
+
+export default Header
